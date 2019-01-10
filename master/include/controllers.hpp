29c11188732724d67bcbd9a36b8eeea1d13e0168
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2019-01-09 17:55:01 -0700

    Flywheel control and speed adjustment code

--- master/include/controllers.hpp
+++ master/include/controllers.hpp
@@ -2,15 +2,41 @@
 
 #include "main.h"
 
+//general variables
+#define MOTOR_GEARSET_06_MAXSPEED 600 //maximum speed for the 36:1 gear set
+#define MOTOR_GEARSET_18_MAXSPEED 200 //maximum speed for the 18:1 gear set
+#define MOTOR_GEARSET_36_MAXSPEED 100 //maximum speed for the 6:1 gear set
+
 //controllers
-extern pros::Controller* master;
+extern Controller* master;
 
 namespace Motors
 {
+  //ports
+  const int PortFlywheel = 1;
 
+  //flywheel
+  extern Motor* flywheel;
 }
 
 namespace Flywheel
 {
+  //are we in flywheel speed finding mode?
+  #define FLYWHEEL_FINE_CONTROL_MODE
+  #define FLYWHEEL_FINE_CONTROL_INCREMENT 5
+
+  //flywheel speeds
+  enum Mode
+  {
+    Stopped,
+    Variable
+  };
+
+  //velocity management variables
+  extern Mode mode;
+  extern int speed;
+  
+  //manage flywheel speed
+  void Controller();
 
 }
