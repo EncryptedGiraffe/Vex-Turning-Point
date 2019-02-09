32d4df94e750069309cd5f73458922dfb92ff3c5
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2019-02-09 10:36:50 -0700

    Setup for autonomous programming

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -1,6 +1,45 @@
 #include "../include/controllers.hpp"
 
+//variables
+int turnDirection = 0;
+
+//function declarations
+void Far();
+void Close();
+
 void autonomous()
 {
   Robot::WriteMessage("Autonomous started!");
+  //check turn direction
+  if(Robot::team == Robot::Team_e_t::Red)
+  {
+    //set rotation
+    turnDirection = 1;
+  }
+  else
+  {
+    //set rotation
+    turnDirection = -1;
+  }
+  //check which autonomous function to run
+  if(Robot::startingTile == Robot::StartingTile_e_t::Close)
+  {
+    //close tile autonomous
+    Close();
+  }
+  else
+  {
+    //far tile autonomous
+    Far();
+  }
+}
+
+void Close()
+{
+
+}
+
+void Far()
+{
+
 }
