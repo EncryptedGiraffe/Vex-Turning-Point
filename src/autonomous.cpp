f8cfda16b124dcd9bac6b246f6f265d2f3c92679
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2019-02-13 15:39:18 -0700

    Skills autonomous

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -12,21 +12,20 @@ void autonomous()
   if(Robot::IsSkills)
   {
     Skills();
+    return;
   }
   //check which autonomous function to run
   if(Robot::startingTile == Robot::StartingTile_e_t::Close)
   {
     //close tile autonomous
     Close();
+    return;
   }
   else
   {
     //far tile autonomous
     Far();
-  }
-  while(true)
-  {
-    pros::delay(20);
+    return;
   }
 }
 
@@ -291,5 +290,82 @@ void Far()
 
 void Skills()
 {
-
+  //////////Fire ball step//////////
+  //set flywheel speed
+  Flywheel::speed = 200;
+  //run its controller to get to that speed
+  Flywheel::Controller();
+  //wait until it gets to that speed
+  //Flywheel::velController.waitUntilSettled();
+  pros::delay(3100);
+  //run the intake to fire the ball
+  Motors::intake.move(127);
+  //wait for the ball to have time to fire
+  pros::delay(1000);
+  //shutdown the intake
+  Motors::intake.move(0);
+  //////////End of fire ball step///////////
+  //////////Toggle low flag step////////////
+  //turn counter clockwise
+  Chassis::controller.turnAngle(-15);
+  pros::delay(100);
+  //drive forward 2 tiles which is 48 inches
+  Chassis::controller.moveDistance(50_in);
+  pros::delay(100);
+  //turn counter clockwise
+  Chassis::controller.turnAngle(-10_deg);
+  pros::delay(100);
+  //drive backward 1 tile which is 24 inches
+  Chassis::controller.moveDistance(-24_in);
+  pros::delay(100);
+  /////////End of toggle low flag step//////////
+  /////////Flip first cap step//////////////////
+  //deploy the flipper
+  Flipper::StartUp();
+  //rotate 90 degress clockwise
+  Chassis::controller.turnAngle(88_deg);
+  //drive forward a small amount
+  //Chassis::controller.moveDistance(10_in);
+  //start moving forward
+  Chassis::controller.moveDistance(12_in);
+  //raise the flipper
+  Flipper::Raise();
+  //////////End of flip first cap step//////////
+  //rotate clockwise
+  Chassis::controller.turnAngle(90_deg);
+  //lower the flipper
+  Flipper::StartUp();
+  //drive forward
+  Chassis::controller.moveDistance(24_in);
+  //rotate counter clock wise
+  Chassis::controller.turnAngle(-105_deg);
+  //drive forward
+  Chassis::controller.moveDistance(12_in);
+  //rotate clockwise
+  Chassis::controller.turnAngle(15_deg);
+  //drive backward
+  Chassis::controller.moveDistance(-36_in);
+  //drive forward
+  Chassis::controller.moveDistance(4_in);
+  //rotate clockwise
+  Chassis::controller.turnAngle(90_deg);
+  //drive forward
+  Chassis::controller.moveDistance(24_in);
+  //rotate counter clockwise
+  Chassis::controller.turnAngle(-90_deg);
+  //drive onto the center platform
+  Chassis::controller.moveDistance(54);
+  //////////Shutdown flywheel step///////////////
+  //set flywheel to stopping
+  Flywheel::mode = Flywheel::Stopped;
+  //run the flywheel controller until the flywheel is stopped
+  for(int i = 0; i < 200; ++i)
+  {
+    Flywheel::Controller();
+    pros::delay(10);
+  }
+  //make sure it is stopped
+  Flywheel::mode = Flywheel::Variable;
+  Flywheel::speed = 0;
+  Flywheel::Controller();
 }
